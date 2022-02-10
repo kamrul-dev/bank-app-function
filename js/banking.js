92e8d1eb979fb5e54178253a3b4cbdd39e146baf
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-10 20:42:43 +0600

    get the value from deposit input field

--- js/banking.js
+++ js/banking.js
@@ -1,4 +1,8 @@
 // button event handler added
 document.getElementById('deposit-btn').addEventListener('click', function(){
-    
+    //get the input value
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmount = depositInput.value;
+    console.log(depositAmount)
+
 }); 
