786602858d0f65d331bb5fd8b16da141cb0e637f
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-10 21:11:38 +0600

    Update deposit and balance and handle string add

--- js/banking.js
+++ js/banking.js
@@ -2,12 +2,22 @@
 document.getElementById('deposit-btn').addEventListener('click', function () {
     //get the input value
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
 
     //get current deposit
     const depositTotal = document.getElementById('deposit-total');
     const depositTotalText = depositTotal.innerText;
-    depositTotal.innerText = depositAmount;
+    const previousDepositTotal = parseFloat(depositTotalText);
+    //existing amount + new anmount = total deposit
+    depositTotal.innerText = previousDepositTotal + depositAmount;
+
+    // update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal + depositAmount;
+
     // clear deposit input field
     depositInput.value = '';
     
